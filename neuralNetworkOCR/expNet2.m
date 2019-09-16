30ba2fe8edcc3e1fd62b17ef82b538fbf6c7c680
coderXmachina2 <technologeeks92@gmail.com>
2019-09-16 20:16:14 +1200

    final commit nature inspired

--- neuralNetworkOCR/expNet2.m
+++ neuralNetworkOCR/expNet2.m
@@ -3,16 +3,18 @@
 
 %load data, load network
 [input, targets] = datStream;
-ffNetFiftyNodes = feedforwardnet(20);
+%ffNetFiftyNodes = feedforwardnet(20);
 
 %view before training
 view(ffNetFiftyNodes);
 
 %train
-[ffNetFiftyNodes, tr] = train(ffNetFiftyNodes, input, targets); %
+[ffNetFiftyNodes, tr] = traingda(ffNetFiftyNodes, input, targets); %
 
 %view after training
 view(ffNetFiftyNodes);
 
 inputConfMat = ffNetFiftyNodes(input);
 perfInput = perform(ffNetFiftyNodes, inputConfMat, targets);
+
+perfSumAbsErr = sae(ffNetFiftyNodes,targets,inputConfMat);
